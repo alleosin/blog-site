b8c2069df478c011dfa59caa6a8e3886d5a07285
Aliaksiej <alleosin@gmail.com>
2024-02-05 18:27:13 +0300

    Added comments and tags

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,12 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
+
 
 class Post(models.Model):
     def get_absolute_url(self):
-        """viartaje kananičny adras abjektu"""
+        """returns the canonical address of the object"""
         return reverse('blog:post_detail',
                         args=[self.publish.year,
                               self.publish.strftime('%m'),
@@ -18,8 +20,8 @@ class Post(models.Model):
         ('published', 'Published'),
     )
     title = models.CharField(max_length=250)
-    #slug - mietka, maje ŭ sabie tolki litary, ličby, padkreślivańni abo złučki
-    #paramietar unique_for_date dazvalaje pabudavać unikalnyja URL-adrasy, što majuć u sabie title pastu j datu jaho publikacyji
+    #slug - label, contains only letters, numbers, underscores, or hyphens
+    #the unique_for_date parameter allows you to build unique URLs that contain the title of the post and the date it was published
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, related_name='blog_posts', on_delete=models.CASCADE)
     body = models.TextField()
@@ -27,9 +29,25 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    tags = TaggableManager()
 
-class Meta:
-    ordering = ('-publish',)
+    class Meta:
+        ordering = ('-publish',)
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path("", views.post_list, name="post_list"),
-    path("", views.PostListView.as_view(), name="post_list"),
+    path("", views.post_list, name="post_list"),
+    #path("", views.PostListView.as_view(), name="post_list"),
+    path(r'^tag/(?P<tag_slug>[-\w]/$', views.post_list, name='post_list_by_tag'),
     path(r"^(P?<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/"\
          r"(?P<post>[-\w]+)/$",
          views.post_detail,
